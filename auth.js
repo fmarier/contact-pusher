5021e0d5198572ed8848f5e0f99a6cb5e4f6976a
Francois Marier <francois@mozilla.com>
2014-03-20 17:49:10 +1300

    Test Evert's new provisioning API

---
+++ auth.js
@@ -0,0 +1,24 @@
+const CONTACTS_URL = 'http://moz.fruux.net';
+
+function reqListener() {
+  var result = document.getElementById('result');
+  result.textContent = this.responseText;
+}
+
+function getCredentials() {
+  navigator.id.get(function (assertion) {
+    var data = {
+      debugaudience: 'http://localhost',
+      assertion: assertion
+    };
+
+    var oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.open('POST', CONTACTS_URL + '/browserid/login', true);
+    oReq.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    oReq.send(JSON.stringify(data));
+  });
+}
+
+var getcreds_button = document.getElementById('getcreds-button');
+getcreds_button.onclick = getCredentials;
